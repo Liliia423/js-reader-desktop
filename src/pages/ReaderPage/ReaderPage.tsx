89dc81a3e03287ca8bb75f66f8e190753276f1f2
Liliia423 <etude4408@gmail.com>
2025-05-22 21:46:04 +0300

    Initial commit

--- src/pages/ReaderPage/ReaderPage.tsx
+++ src/pages/ReaderPage/ReaderPage.tsx
@@ -11,31 +11,8 @@ export const ReaderPage = () => {
   };
 
   return (
-    <div style={{ padding: "1rem" }}>
-      {/* Стилізована кнопка */}
-      <label htmlFor="pdfUpload" style={{ cursor: "pointer" }}>
-        <button style={{ padding: "0.5rem 1rem", fontSize: "1rem" }}>
-          📂 Open new file
-        </button>
-      </label>
-
-      {/* Невидиме поле вибору файлу */}
-      <input
-        id="pdfUpload"
-        type="file"
-        accept=".pdf"
-        onChange={handleFileChange}
-        style={{ display: "none" }}
-      />
-
-      {/* Назва відкритого файлу */}
-      {file && (
-        <div style={{ marginTop: "1rem", fontStyle: "italic", color: "#555" }}>
-          📄 Відкрито: <strong>{file.name}</strong>
-        </div>
-      )}
-
-      {/* PDF-переглядач */}
+    <div>
+      <input type="file" accept=".pdf" onChange={handleFileChange} />
       {file && <PdfViewer file={file} />}
     </div>
   );
