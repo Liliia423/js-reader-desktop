85914b0e508a85c421396e0464f190992b3c34a4
Liliia423 <etude4408@gmail.com>
2025-05-25 16:51:47 +0300

    Initial commit

--- src/components/PdfViewer/PdfViewer.tsx
+++ src/components/PdfViewer/PdfViewer.tsx
@@ -81,67 +81,70 @@ export const PdfViewer = ({ file }: Props) => {
 
   return (
     <div>
-      <div className={styles.pdfWrapper}>
-        <Document file={fileUrl} onLoadSuccess={onDocumentLoadSuccess}>
-          <Page pageNumber={pageNumber} width={pageWidth} />
-        </Document>
-      </div>
-      <div style={{ marginTop: "10px" }}>
-        <button onClick={() => setPageNumber((p) => Math.max(p - 1, 1))}>
-          ←
-        </button>
-        <span style={{ margin: "0 10px" }}>
-          Page {pageNumber} of {numPages}
-        </span>
-        <button
-          onClick={() => setPageNumber((p) => Math.min(p + 1, numPages || 1))}
-        >
-          →
-        </button>
-      </div>
-
-      {bookmarks.length > 0 && (
-        <div className={styles.bookmarksBlock}>
-          <button className={styles.iconsBottom} onClick={addBookmark}>
-            <svg
-              className={`${styles.bookmarkIcon} ${
-                isBookmarked ? styles.bookmarkIconActive : ""
-              }`}
-              width="20"
-              height="27"
-              viewBox="0 0 20 27"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M1.25 0.5H18.75C19.0815 0.5 19.3995 0.631696 19.6339 0.866116C19.8683 1.10054 20 1.41848 20 1.75V25.6787C20.0001 25.7905 19.9703 25.9003 19.9136 25.9966C19.8569 26.0929 19.7755 26.1722 19.6777 26.2264C19.5799 26.2805 19.4694 26.3074 19.3577 26.3043C19.246 26.3012 19.1371 26.2682 19.0425 26.2087L10 20.5375L0.9575 26.2075C0.86296 26.2669 0.754234 26.2999 0.642627 26.303C0.531021 26.3062 0.420608 26.2794 0.322869 26.2254C0.22513 26.1714 0.143634 26.0923 0.0868535 25.9961C0.0300735 25.9 8.30886e-05 25.7904 0 25.6787V1.75C0 1.41848 0.131696 1.10054 0.366117 0.866116C0.600537 0.631696 0.918479 0.5 1.25 0.5Z" />
-            </svg>
-          </button>
-
-          <strong>Bookmarks:</strong>
-          <div
-            style={{
-              display: "flex",
-              flexWrap: "wrap",
-              gap: "10px",
-              marginTop: "5px",
-            }}
-          >
-            {bookmarks.map((page) => (
-              <div
-                key={page}
-                style={{ display: "flex", alignItems: "center", gap: "5px" }}
-              >
-                <button onClick={() => setPageNumber(page)}>{page}</button>
-                <button
-                  onClick={() => removeBookmark(page)}
-                  style={{ color: "red" }}
-                >
-                  ✕
-                </button>
-              </div>
-            ))}
+      <div className={styles.firstBlock}>
+        {bookmarks.length > 0 && (
+          <div className={styles.bookmarksFirstBlock}>
+            <strong className={styles.boomarks}>Bookmarks:</strong>
+            <div className={styles.selectBookmarks} style={{}}>
+              {bookmarks.map((page) => (
+                <div className={styles.actionBookmarks} key={page}>
+                  <button
+                    className={styles.bookmarkBtn}
+                    onClick={() => setPageNumber(page)}
+                  >
+                    {page}
+                  </button>
+                  <button
+                    className={styles.removeBtn}
+                    onClick={() => removeBookmark(page)}
+                    style={{ color: "red" }}
+                  >
+                    ✕
+                  </button>
+                </div>
+              ))}
+            </div>
           </div>
+        )}
+        <div className={styles.pdfWrapper}>
+          <Document file={fileUrl} onLoadSuccess={onDocumentLoadSuccess}>
+            <Page pageNumber={pageNumber} width={pageWidth} />
+          </Document>
         </div>
-      )}
+      </div>
+
+      <div className={styles.pagesAndMark}>
+        <div className={styles.arrows}>
+          <button
+            className={styles.navBtn}
+            onClick={() => setPageNumber((p) => Math.max(p - 1, 1))}
+          >
+            ←
+          </button>
+          <span className={styles.pageLabel} style={{ margin: "0 10px" }}>
+            Page {pageNumber} of {numPages}
+          </span>
+          <button
+            className={styles.navBtn}
+            onClick={() => setPageNumber((p) => Math.min(p + 1, numPages || 1))}
+          >
+            →
+          </button>
+        </div>
+        <button className={styles.iconsBottom} onClick={addBookmark}>
+          <svg
+            className={`${styles.bookmarkIcon} ${
+              isBookmarked ? styles.bookmarkIconActive : ""
+            }`}
+            width="20"
+            height="27"
+            viewBox="0 0 20 27"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M1.25 0.5H18.75C19.0815 0.5 19.3995 0.631696 19.6339 0.866116C19.8683 1.10054 20 1.41848 20 1.75V25.6787C20.0001 25.7905 19.9703 25.9003 19.9136 25.9966C19.8569 26.0929 19.7755 26.1722 19.6777 26.2264C19.5799 26.2805 19.4694 26.3074 19.3577 26.3043C19.246 26.3012 19.1371 26.2682 19.0425 26.2087L10 20.5375L0.9575 26.2075C0.86296 26.2669 0.754234 26.2999 0.642627 26.303C0.531021 26.3062 0.420608 26.2794 0.322869 26.2254C0.22513 26.1714 0.143634 26.0923 0.0868535 25.9961C0.0300735 25.9 8.30886e-05 25.7904 0 25.6787V1.75C0 1.41848 0.131696 1.10054 0.366117 0.866116C0.600537 0.631696 0.918479 0.5 1.25 0.5Z" />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 };
