aeb44ba8c9bccdb35ef6fce44a2c6316639b410a
Liliia423 <etude4408@gmail.com>
2025-05-28 19:34:39 +0300

    Initial commit

--- src/components/BookmarkPreviewModal/BookmarkPreviewModal.tsx
+++ src/components/BookmarkPreviewModal/BookmarkPreviewModal.tsx
@@ -5,28 +5,19 @@ type Props = {
   file: File;
   page: number;
   onClose: () => void;
-  onDelete: (page: number) => void;
 };
 
-export const BookmarkPreviewModal = ({
-  file,
-  page,
-  onClose,
-  onDelete,
-}: Props) => {
+export const BookmarkPreviewModal = ({ file, page, onClose }: Props) => {
   return (
     <div className={styles.modal} onMouseLeave={onClose}>
-      <div className={styles.topBar}>
-        <button className={styles.closeBtn} onClick={onClose}>
-          ✕
-        </button>
-        <button className={styles.deleteBtn} onClick={() => onDelete(page)}>
-          ✗
-        </button>
-      </div>
       <div className={styles.preview}>
         <Document file={file}>
-          <Page pageNumber={page} width={200} renderTextLayer={false} />
+          <Page
+            pageNumber={page}
+            width={600}
+            renderTextLayer={false}
+            renderAnnotationLayer={false}
+          />
         </Document>
         <div className={styles.pageLabel}>Page {page}</div>
       </div>
